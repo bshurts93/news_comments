9ce3a1cd7442085f5fde30daa8f3678d65a71e82
Ben Shurts <bshurts93@gmail.com>
2019-05-29 14:09:53 -0500

    Get route w/article id now populates JSON with associated comments

--- server.js
+++ server.js
@@ -148,41 +148,11 @@ app.post("/submit", function(req, res) {
 
 app.get("/articles/:id", function(req, res) {
   db.Article.findOne({ _id: req.params.id })
-    .populate("comment")
+    .populate("comments")
     .then(function(dbArticle) {
       res.json(dbArticle);
     })
     .catch(function(err) {
-      res.json(err);
-    });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  var data = {
-    user: "Ben",
-    text: req.body.comment
-  };
-
-  console.log(data);
-  db.Comment.create(data)
-    .then(function(dbComment) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      // return db.Article.findOneAndUpdate(
-      //   { _id: req.params.id },
-      //   { comment: dbComment._id },
-      //   { new: true }
-      // );
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
